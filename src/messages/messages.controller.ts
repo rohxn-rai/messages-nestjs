9aaa0a1f79588c20f532deebf82cc3ab9ba90f79
rohxn-rai <rohan.rai932@outlook.com>
2025-09-02 11:22:49 -0400

    greated a basic message controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
+import type { UUID } from "crypto";
 
-@Controller('messages')
-export class MessagesController {}
+@Controller("messages")
+export class MessagesController {
+  @Get()
+  listMessage() {
+    return "listMessage";
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    console.log(body);
+    return "postMessage";
+  }
+
+  @Get("/:id")
+  getMessage(@Param("id") id: UUID) {
+    return `perticularMessage ${id}`;
+  }
+}
